55f612971ec7345cbbb36160ef00f05518738b8b
Valentin <valko27@abv.bg>
2024-12-15 15:23:11 +0200

    Create components for navigation and footer

--- client/ecommerce-shop/src/app/core/components/nav/nav.component.ts
+++ client/ecommerce-shop/src/app/core/components/nav/nav.component.ts
@@ -1,5 +1,5 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { UserService } from '../services/user.service';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-nav',
