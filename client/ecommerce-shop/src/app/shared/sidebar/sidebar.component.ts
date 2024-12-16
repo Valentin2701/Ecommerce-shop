0a08604e7a2bbf1e877a2f2c3f9005995a3851e2
Valentin <valko27@abv.bg>
2024-12-16 19:40:33 +0200

    Add routes on sidebar

--- client/ecommerce-shop/src/app/shared/sidebar/sidebar.component.ts
+++ client/ecommerce-shop/src/app/shared/sidebar/sidebar.component.ts
@@ -1,8 +1,11 @@
 import { Component, Input } from '@angular/core';
+import { UserService } from 'src/app/core/services/user.service';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.css']
 })
-export class SidebarComponent {}
+export class SidebarComponent {
+  constructor(public userService: UserService){}
+}
