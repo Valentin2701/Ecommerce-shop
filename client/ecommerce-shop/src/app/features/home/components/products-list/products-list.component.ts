85c1f8c75d92efa6e1ede061709f0d170f8fd1ad
Valentin <valko27@abv.bg>
2024-12-17 23:49:33 +0200

    Fetch products on home page

--- client/ecommerce-shop/src/app/features/home/components/products-list/products-list.component.ts
+++ client/ecommerce-shop/src/app/features/home/components/products-list/products-list.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { ApiService } from 'src/app/core/services/api.service';
+import { Product } from 'src/app/core/types/Product';
 
 @Component({
   selector: 'app-products-list',
   templateUrl: './products-list.component.html',
   styleUrls: ['./products-list.component.css']
 })
-export class ProductsListComponent {
-  
+export class ProductsListComponent implements OnInit {
+  public rows: Product[][] = [];
+
+  constructor(private productService: ApiService) { }
+
+  ngOnInit(): void {
+    this.productService.getRecentProducts().subscribe((prod: Product[]) => this.rows = this.getRows(prod));
+  }
+
+  private getRows(products: Product[]): Product[][] {
+    const rows: Product[][] = [];
+    for (let i = 0; i < products.length; i++) {
+      if (i % 2 == 0) rows.push([products[i]]);
+      else rows[(i - 1 / 2)].push(products[i]);
+    }
+    return rows
+  }
 }
--- client/ecommerce-shop/src/app/features/login/login.module.ts
+++ client/ecommerce-shop/src/app/features/login/login.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login.component';
 import { ReactiveFormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from 'src/app/app-routing.module';
 
 
